b99d9a833e3dd12aeb1504e8a7a95ccc1ce73502
mlobf <marcoslemeborbafilho@gmail.com>
2021-07-28 15:45:13 -0300

    More concise explanation about nodes's async basics

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+// Just a simples test using the most basic
+//  features at Node.js
+
 console.log("Starting");
 
 setTimeout(() => {
-  console.log("2 second timer");
+  console.log("Two second ");
 }, 2000);
 
 setTimeout(() => {
-  console.log("0 second timer");
-}, 0); 
+  console.log("Zero seconds");
+}, 0);
 
-console.log("Stopping");
-
+console.log("Finishing up");
